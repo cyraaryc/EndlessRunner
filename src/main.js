fa2a9193b3b34fef4a4f36ecacb88618357445f7
cyraaryc <bwitten@ucsc.edu>
2025-02-10 22:39:27 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@
 //https://freesound.org/people/Druminfected/sounds/250551/ cc-by 0 - audio source 2
 //Creative Tilt - I liked the creative direction I went with this game, in terms of the music matching the gameplay in a very direct way.
 //Usually, when I make games, or play games, the music is largley tangential to the gameplay, even in endless runners.
-//I wanted to go a different way, as I view endless runners and rhythm games as very similar, in terms of skills and player exp., so I wanted to replicate a rhytm game exp. in the form of an endless runner
+//I wanted to go a different way, as I view endless runners and rhythm games as very similar, in terms of skills and player exp,
+//so I wanted to replicate a rhythm game exp. in the form of an endless runner.
 //The rhythm heaven games greatly inspired me, and I felt I was able to capture some of the vibes of those games in an endless runner form. 
 //On the technical side, I learned alot about basic platformer movement, gravity, and input buffering, all of which contributed to my final game.
-//Tinkering with gravity and controls beyonds the basic platfomer mold is something I am proud of, in terms of increasing and decreasing gravity, along with input buffering.
+//Tinkering with gravity and controls beyond a basic platfomer is something I am proud of, in terms of increasing and decreasing gravity, along with input buffering.
 let config = {
     type: Phaser.AUTO,
     width: 1340,
