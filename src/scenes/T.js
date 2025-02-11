bcc8217b43fa18f722dd883bc0578373fa76c7a9
cyraaryc <bwitten@ucsc.edu>
2025-02-10 23:24:17 -0800

    Update

--- src/scenes/T.js
+++ src/scenes/T.js
@@ -31,7 +31,7 @@ class T extends Phaser.Scene {
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*1, 'The game is endless, and the BPM, as well as speed, increases over time', menuConfig).setOrigin(0.5)
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*1.5, 'When you lose, the level resets', menuConfig).setOrigin(0.5)
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*2, 'The BPM tracks the speed of the track', menuConfig).setOrigin(0.5)
-    this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*2.5, 'Hit the power ups to increase your BPM, for easier gameplay, avoid them', menuConfig).setOrigin(0.5)
+    this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*2.5, 'Hit the powerups to increase your BPM, for easier gameplay, avoid them', menuConfig).setOrigin(0.5)
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*3, 'Press M while in-game to go back to the main menu', menuConfig).setOrigin(0.5)
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*3.5, 'Credits:https://freesound.org/people/Druminfected/sounds/250551/ cc-by 0 - audio source', menuConfig).setOrigin(0.5)
     this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding)*4, 'https://sonniss.com/gameaudiogdc - audio source 2', menuConfig).setOrigin(0.5)
